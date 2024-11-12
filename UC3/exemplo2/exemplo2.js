851348df85e266c2a0b47992d67492e0618b4174
Caio Marques <caioherorj@gmail.com>
2024-11-11 21:55:20 -0300

    asda

---
+++ UC3/exemplo2/exemplo2.js
@@ -0,0 +1,19 @@
+document.write('ola terra')
+alert('ola')
+console.log('olá, tudo bem')
+var variavel1 = 'ola mundo';
+alert(variavel1);
+var variavel2 = 123;
+alert(variavel2)
+console.log(variavel2);
+let variavel3 = 'Eu também sou uma variavel';
+
+//alert(variavel3);
+
+/* comentario
+em javascript
+em multiplas linhas
+*/
+document.write('olá marte')
+document.write('<h1> eu sou um titulo </h1>')
+document.write('<title> titulo da pagina </title>')
